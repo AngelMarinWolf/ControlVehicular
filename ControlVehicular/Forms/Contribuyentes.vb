1db92cda679e99823fa42eca5b19d752af58a6f6
selenatorresmireles <50244098+selenatorresmireles@users.noreply.github.com>
2019-05-10 11:16:23 -0500

    Add files via upload

---
+++ ControlVehicular/Forms/Contribuyentes.vb
@@ -0,0 +1,9 @@
+﻿Public Class Contribuyentes
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Materno_Click(sender As Object, e As EventArgs) Handles x6.Click
+
+    End Sub
+End Class
---
+++ ControlVehicular/Forms/Multas.vb
@@ -0,0 +1,33 @@
+﻿Public Class Multas
+    Private Sub Multas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TieneLicencia_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Si_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub No_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Fecha_Click(sender As Object, e As EventArgs) Handles Fecha.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
